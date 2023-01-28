98bfcb617c7f193653f55a559db44afc8243563d
Mehsain <114495317+mehsain@users.noreply.github.com>
2023-01-28 12:30:46 +0600

    .

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [],
+  theme: {
+    extend: {      
+      backgroundImage: {
+      'hero-pattern': "url('/img/pattern1.png')",
+    }},
+  },
+  plugins: [],
+
+
+}
